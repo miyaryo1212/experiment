d242a7328826a2b14d48fcfd9d3943760f226d39
miyaryo1212 <83214456+miyaryo1212@users.noreply.github.com>
2022-05-29 16:02:38 +0900

    #

---
+++ web-site-vr-master@anharu2394/web-site/script.js
@@ -0,0 +1,22 @@
+  var home = document.querySelector('#home');
+  var about = document.querySelector('#about');
+  var service = document.querySelector('#service');
+  var contact = document.querySelector('#contact');
+
+
+  home.addEventListener('click', function () {
+    location.href = "/web-site-vr/web-site/";
+  });
+
+  about.addEventListener('click', function () {
+    location.href = "/web-site-vr/web-site/about.html";
+  });
+
+  service.addEventListener('click', function () {
+    location.href = "/web-site-vr/web-site/service.html";
+  });
+
+  contact.addEventListener('click', function () {
+    location.href = "/web-site-vr/web-site/contact.html";
+  });
+
